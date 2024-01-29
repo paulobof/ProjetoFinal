2d2b2667dafde501240bb058ce6ed091b984f929
paulobof <paulobof@gmail.com>
2024-01-29 14:48:23 -0300

    Inicio da Criação de Produtos

---
+++ src/models/Products.js
@@ -0,0 +1,38 @@
+class User{
+    constructor(
+        name,
+        type,
+        model,
+        description,
+        picture
+    ){
+        this.name = name,
+        this.type = type,
+        this.model = model,
+        this.description = description,
+        this.picture = picture        
+    }
+
+    getName(){
+        return this.name;
+    }
+
+    getType(){
+        return this.type;
+    }
+
+    getModel(){
+        return this.model;
+    }
+
+    getDescription(){
+        return this.description;
+    }
+
+    getPicture(){
+        return this.picture;
+    }
+
+}
+
+module.exports = User
